814079a4a462475ca51cc0448b8b43318ca4122c
Sajal Kayan <sajal83@gmail.com>
2024-05-05 17:24:57 +0700

    Each test file (or even function) can define its own state requirements

--- tests_boa/conftest.py
+++ tests_boa/conftest.py
@@ -3,18 +3,12 @@ import os
 import pytest
 
 import boa
-from boa.environment import Env
 
 
-# run all tests with this forked environment
-# called as fixture for its side effects
-@pytest.fixture(scope="module", autouse=True)
-def forked_env_mainnet():
-    with boa.swap_env(Env()):
-        alchemy_key = os.environ.get("WEB3_ALCHEMY_API_KEY")
-        if alchemy_key == "":
-            raise "WEB3_ALCHEMY_API_KEY must be provided"
-        fork_uri =  "https://eth-mainnet.g.alchemy.com/v2/" + alchemy_key
-        block_id = 17024800  # some block we know the state of
-        boa.env.fork(fork_uri, block_identifier=block_id)
-        yield
+def forked_env_mainnet(block_id: int):
+    alchemy_key = os.environ.get("WEB3_ALCHEMY_API_KEY")
+    if alchemy_key == "":
+        raise "WEB3_ALCHEMY_API_KEY must be provided"
+    fork_uri =  "https://eth-mainnet.g.alchemy.com/v2/" + alchemy_key
+    boa.env.fork(fork_uri, block_identifier=block_id)
+        
--- tests_boa/fork.py
+++ tests_boa/fork.py
@@ -4,8 +4,12 @@ import boa
 
 from tests_boa.conftest import forked_env_mainnet
 
+@pytest.fixture
+def setup_chain():
+    #tests in this file require mainnet block 17024800
+    forked_env_mainnet(17024800)
 
-def test_vitalik_balance(forked_env_mainnet):
+def test_vitalik_balance(setup_chain):
     vitaliks_loot = boa.env.get_balance(addr="0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045")
     print(vitaliks_loot)
     assert vitaliks_loot == 5149621640741634567741, "Vitalik is rugging EVM state"
