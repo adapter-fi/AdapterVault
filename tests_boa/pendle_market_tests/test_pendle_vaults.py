4fd389d4eaef97898fb30222424507f828a50640
Sajal Kayan <sajal83@gmail.com>
2024-05-13 16:00:07 +0700

    generalized market tests

--- tests_boa/pendle_market_tests/test_pendle_vaults.py
+++ tests_boa/pendle_market_tests/test_pendle_vaults.py
@@ -10,9 +10,6 @@ from decimal import Decimal
 
 PENDLE_ROUTER="0x00000000005BBB0EF59571E58418F9a4357b68A0"
 PENDLE_ROUTER_STATIC="0x263833d47eA3fA4a30f269323aba6a107f9eB14C"
-PENDLE_MARKET="0x9C73879F795CefA1D5239dE08d1B6Aba2D2d1434" #Pendle: PT-stETH-26DEC24/SY-stETH Market Token
-ENA="0x57e114B691Db790C35207b2e685D4A43181e6061"
-PENDLE_PT="0x9946C55a34CD105f1e0CF815025EAEcff7356487"
 PENDLE_ORACLE="0x66a1096C6366b2529274dF4f5D8247827fe4CEA8"
 
 MAX_UINT256 = 115792089237316195423570985008687907853269984665640564039457584007913129639935
@@ -38,12 +35,30 @@ def trader(setup_chain):
     boa.env.set_balance(acc, 1000*10**18)
     return acc
 
-@pytest.fixture
-def ena(setup_chain, trader):
+def _steth(trader, addr):
     with open("contracts/vendor/IERC20.json") as f:
         j = json.load(f)
     factory = boa.loads_abi(json.dumps(j["abi"]), name="ERC20")
-    en = factory.at(ENA)
+    st = factory.at(addr)
+    #Trader needs to acquire some ETH, dunno how
+    abi_encoded = eth_abi.encode(['address', 'uint256'], [trader, 0])
+    storage_slot = Web3.solidity_keccak(["bytes"], ["0x" + abi_encoded.hex()])
+
+    boa.env.set_storage(
+        boa.util.abi.Address(addr).canonical_address,
+        Web3.to_int(storage_slot),
+        5000 * 10**18
+    )
+    print(st.balanceOf(trader))
+    print(boa.env.get_balance(trader))
+    assert st.balanceOf(trader) > 5000 * 10**18, "Trader did not get 'airdrop'"
+    return st
+
+def _ena(trader, addr):
+    with open("contracts/vendor/IERC20.json") as f:
+        j = json.load(f)
+    factory = boa.loads_abi(json.dumps(j["abi"]), name="ERC20")
+    en = factory.at(addr)
     #Trader needs to acquire some ETH, dunno how
     abi_encoded = eth_abi.encode(['address', 'uint256'], [trader, 2])
     storage_slot = Web3.solidity_keccak(["bytes"], ["0x" + abi_encoded.hex()])
@@ -58,7 +73,7 @@ def ena(setup_chain, trader):
 
     # return
     boa.env.set_storage(
-        ENA,
+        addr,
         Web3.to_int(storage_slot),
         5000 * 10**18
     )
@@ -67,35 +82,32 @@ def ena(setup_chain, trader):
     assert en.balanceOf(trader) == 5000 * 10**18, "Trader did not get 'airdrop'"
     return en
 
-@pytest.fixture
-def pendleOracle(setup_chain, pendleMarket):
+def _pendleOracle(pendleMarket, _PENDLE_ORACLE):
     with open("contracts/vendor/PendlePtLpOracle.json") as f:
         j = json.load(f)
     factory = boa.loads_abi(json.dumps(j["abi"]), name="PendlePtLpOracle")
-    oracle = factory.at(PENDLE_ORACLE)
-    increaseCardinalityRequired, cardinalityRequired, oldestObservationSatisfied = oracle.getOracleState(PENDLE_MARKET, 1200)
+    oracle = factory.at(_PENDLE_ORACLE)
+    increaseCardinalityRequired, cardinalityRequired, oldestObservationSatisfied = oracle.getOracleState(pendleMarket, 1200)
     if increaseCardinalityRequired:
         pendleMarket.increaseObservationsCardinalityNext(cardinalityRequired)
         print("cardinality increased")
     #just in case, simulate passage of time
     boa.env.time_travel(seconds=1200)
-    increaseCardinalityRequired, cardinalityRequired, oldestObservationSatisfied = oracle.getOracleState(PENDLE_MARKET, 1200)
+    increaseCardinalityRequired, cardinalityRequired, oldestObservationSatisfied = oracle.getOracleState(pendleMarket, 1200)
     print(increaseCardinalityRequired, cardinalityRequired, oldestObservationSatisfied)
     assert increaseCardinalityRequired==False, "increaseCardinality failed"
     assert oldestObservationSatisfied==True, "oldestObservation is not Satisfied"
     return oracle
 
-@pytest.fixture
-def pendleMarket(setup_chain):
+def pendle_Market(_pendle_market):
     with open("contracts/vendor/IPMarketV3.json") as f:
         j = json.load(f)
     factory = boa.loads_abi(json.dumps(j["abi"]), name="IPMarketV3")
-    return factory.at(PENDLE_MARKET)
+    return factory.at(_pendle_market)
 
-@pytest.fixture
-def pendle_adapter(setup_chain, deployer, ena, pendleOracle):
+def _pendle_adapter(deployer, asset, _pendle_market):
     with boa.env.prank(deployer):
-        pa = boa.load("contracts/adapters/PendleAdapter.vy", ena, PENDLE_ROUTER, PENDLE_ROUTER_STATIC, PENDLE_MARKET, PENDLE_ORACLE)
+        pa = boa.load("contracts/adapters/PendleAdapter.vy", asset, PENDLE_ROUTER, PENDLE_ROUTER_STATIC, _pendle_market, PENDLE_ORACLE)
     return pa
 
 @pytest.fixture
@@ -104,15 +116,14 @@ def funds_alloc(setup_chain, deployer):
         f = boa.load("contracts/FundsAllocator.vy")
     return f
 
-@pytest.fixture
-def adaptervault(setup_chain, deployer, ena, trader, funds_alloc):
+def _adaptervault(deployer, asset, trader, funds_alloc):
     with boa.env.prank(deployer):
         v = boa.load(
             "contracts/AdapterVault.vy",
             "ena-Pendle",
             "pena",
             18,
-            ENA,
+            asset,
             [],
             deployer,
             funds_alloc,
@@ -121,15 +132,19 @@ def adaptervault(setup_chain, deployer, ena, trader, funds_alloc):
     return v
 
 
-@pytest.fixture
-def pt(setup_chain):
+def pendle_pt(_pendle_pt):
     with open("contracts/vendor/IERC20.json") as f:
         j = json.load(f)
         factory = boa.loads_abi(json.dumps(j["abi"]), name="ERC20")
-        return factory.at(PENDLE_PT)
+        return factory.at(_pendle_pt)
 
+def market_test(_pendle_pt, asset, trader, deployer, _pendle_market, funds_alloc, _PENDLE_ORACLE):
 
-def test_markets(setup_chain, ena, trader, adaptervault, pendle_adapter, deployer, pendleOracle, pt, pendleMarket):
+    pt = pendle_pt(_pendle_pt)
+    pendleMarket = pendle_Market(_pendle_market)
+    pendleOracle = _pendleOracle(pendleMarket, _PENDLE_ORACLE)
+    pendle_adapter = _pendle_adapter(deployer, asset, _pendle_market)
+    adaptervault = _adaptervault(deployer, asset, trader, funds_alloc)
     strategy = [(ZERO_ADDRESS,0)] * MAX_ADAPTERS 
     strategy[0] = (pendle_adapter.address, 1)
 
@@ -139,12 +154,12 @@ def test_markets(setup_chain, ena, trader, adaptervault, pendle_adapter, deploye
         assert ret == True
     
     with boa.env.prank(trader):
-        ena.approve(adaptervault, 1*10**18)
-        bal_pre = ena.balanceOf(trader)
-        ex_rate = pendleOracle.getPtToAssetRate(PENDLE_MARKET, 1200)
+        asset.approve(adaptervault, 1*10**18)
+        bal_pre = asset.balanceOf(trader)
+        ex_rate = pendleOracle.getPtToAssetRate(_pendle_market, 1200)
         adaptervault.deposit(1*10**18, trader)
         print("GAS USED FOR PENDLE DEPOSIT = ", adaptervault._computation.net_gas_used) 
-        deducted = bal_pre - ena.balanceOf(trader)
+        deducted = bal_pre - asset.balanceOf(trader)
         print(deducted)
         assert deducted == 1*10**18, "Invalid amount got deducted"
         print(pt.balanceOf(adaptervault))
@@ -181,8 +196,8 @@ def test_markets(setup_chain, ena, trader, adaptervault, pendle_adapter, deploye
         assert boa.env.evm.patch.timestamp > pendleMarket.expiry()
         print(adaptervault.totalAssets())
         pt_bal_pre = pt.balanceOf(adaptervault)
-        ena_bal_pre = ena.balanceOf(adaptervault)
-        trader_bal_pre = ena.balanceOf(trader)
+        asset_bal_pre = asset.balanceOf(adaptervault)
+        trader_bal_pre = asset.balanceOf(trader)
         assert adaptervault.balanceOf(trader) == adaptervault.convertToShares( adaptervault.convertToAssets(adaptervault.balanceOf(trader)))
         adaptervault.eval("self.vault_asset_balance_cache=0")
         adaptervault.eval("self.total_asset_balance_cache=0")
@@ -193,21 +208,41 @@ def test_markets(setup_chain, ena, trader, adaptervault, pendle_adapter, deploye
             trader
         )
         pt_bal_post = pt.balanceOf(adaptervault)
-        ena_bal_post = ena.balanceOf(adaptervault)
-        trader_bal_post = ena.balanceOf(trader)
+        asset_bal_post = asset.balanceOf(adaptervault)
+        trader_bal_post = asset.balanceOf(trader)
         #The total PT burned should equal total stETH gained by both trader and vault
         pt_burned = pt_bal_pre - pt_bal_post
         print("pt_burned: ", pt_burned)
-        trader_ena_gained = trader_bal_post - trader_bal_pre
-        print("trader_ena_gained: ", trader_ena_gained)
-        vault_ena_gained = ena_bal_post - ena_bal_pre
-        print("vault_ena_gained: ", vault_ena_gained)
-        print("total gained: ", trader_ena_gained + vault_ena_gained)
+        trader_asset_gained = trader_bal_post - trader_bal_pre
+        print("trader_asset_gained: ", trader_asset_gained)
+        vault_asset_gained = asset_bal_post - asset_bal_pre
+        print("vault_asset_gained: ", vault_asset_gained)
+        print("total gained: ", trader_asset_gained + vault_asset_gained)
         #Theres always rounding issues somewhere...
-        assert pt_burned == pytest.approx(trader_ena_gained + vault_ena_gained), "withdraw was not pegged"
+        assert pt_burned == pytest.approx(trader_asset_gained + vault_asset_gained), "withdraw was not pegged"
 
         #Lets try a deposit, it all should goto cash
-        ena.approve(adaptervault, 1*10**18)
+        asset.approve(adaptervault, 1*10**18)
         adaptervault.deposit(1*10**18, trader)
-        assert ena.balanceOf(adaptervault) - ena_bal_post == pytest.approx(10**18), "ena not gained sufficiently"
+        assert asset.balanceOf(adaptervault) - asset_bal_post == pytest.approx(10**18), "asset not gained sufficiently"
         assert pt.balanceOf(adaptervault) - pt_bal_post == 0, "managed to deposit to PT post-maturity"
+
+
+
+def test_markets_ena(setup_chain, trader, deployer, funds_alloc):
+    #1. ENA on mainnet
+    PENDLE_MARKET="0x9C73879F795CefA1D5239dE08d1B6Aba2D2d1434"
+    ENA="0x57e114B691Db790C35207b2e685D4A43181e6061"
+    PENDLE_PT="0x9946C55a34CD105f1e0CF815025EAEcff7356487"
+    ena = _ena(trader, ENA)
+    market_test(PENDLE_PT, ena, trader, deployer, PENDLE_MARKET, funds_alloc, PENDLE_ORACLE)
+
+def test_markets_steth(setup_chain, trader, deployer, funds_alloc):
+    #2. stETH on mainnet
+    PENDLE_MARKET="0xd0354d4e7bcf345fb117cabe41acadb724eccca2" #Pendle: PT-stETH-26DEC24/SY-stETH Market Token
+    STETH="0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
+    PENDLE_PT="0x7758896b6AC966BbABcf143eFA963030f17D3EdF"
+    steth = _steth(trader, STETH)
+    market_test(PENDLE_PT, steth, trader, deployer, PENDLE_MARKET, funds_alloc, PENDLE_ORACLE)
+
+
